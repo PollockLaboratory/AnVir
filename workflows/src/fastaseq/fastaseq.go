9737ba789b607fff165b29524194daaaf0738d2f
Murad <mchowdh200@gmail.com>
2022-04-13 12:51:51 -0600

    finished windowed reference structure and tested

--- workflows/src/fastaseq/fastaseq.go
+++ workflows/src/fastaseq/fastaseq.go
@@ -1,52 +1,67 @@
 package fastaseq
 
 import (
+	// "fmt"
 	"bufio"
 	"os"
 	"strings"
 	"annotation/utils"
 )
-/*
-   =============================================================================
-   This package provides a datastructure for querying the viral
-   genome by k-mer sequence windows, or contiunously (essentially simple
-   wrapper over a string).
+// =============================================================================
+// This package provides a datastructure for querying the viral
+// genome by k-mer sequence windows, or contiunously (essentially simple
+// wrapper over a string).
+// =============================================================================
 
-   /// Sketch to load sliding windows into interval map
-   * get fasta path, kmer size
-   * declare kmer2coords mapping string to pair of ints
-   * parse fasta into single string -> ref string
-   * for i = 0 to len(ref) - k
-       window = ref[i:i+k] (0-based half-open interval)
-       kmer2coords[window] = interval{i+1, i+k} (1-based closed interval)
-   =============================================================================
-*/
 
-type interval struct {
-	start int
-	end int
+// ============================================================================
+/// Helper structs
+// ============================================================================
+
+type Interval struct {
+	Start int
+	End   int
 }
+
+// ============================================================================
+/// Windowed Reference
+// ============================================================================
 
 // Data structure to store a single fasta record (viral genome) and enable
 // queries keyed by kmer sliding widnows.
-type FastaSeqWindowed struct {
-	Contig string
-	K int
-	Kmer2coords map[string]interval
+type WindowedReference struct {
+	Contig      string
+	K           int
+	Kmer2coords map[string][]Interval
 }
-func (this *FastaSeqWindowed)Query(seq string) (int, int) {
-	i := this.Kmer2coords[seq]
-	return i.start, i.end
+
+// Given a kmer sequence and the genomic positions, add to
+// the data struture.  If location doesn't already exist,
+// instantiate a slice keyed by the seq.
+func (fs *WindowedReference)addseq(seq string, loc Interval) {
+	if l, exists := fs.Kmer2coords[seq]; exists {
+		fs.Kmer2coords[seq] = append(l, loc)
+	} else {
+		fs.Kmer2coords[seq] = []Interval{loc}
+	}
+}
+
+// Query the data structure with a kmer seq.  I'm not enforcing that
+// k-mer is the right size, however the query won't be successful if
+// k isn't correct.  Returns start/end genomic coords (1-based, closed)
+func (fs *WindowedReference)Query(seq string) []Interval {
+	return fs.Kmer2coords[seq]
 }
 
 // Load reference genome (single fasta record) for kmer window queries.
-func LoadRefWindowed(fasta_path string, k int) *FastaSeqWindowed {
+// Serves as the constructor for WindowedReference objects.
+func LoadWindowedReference(fasta_path string, k int) *WindowedReference {
 	f, err := os.Open(fasta_path)
 	utils.Check(err)
 	defer f.Close()
 
-	Ref := new(FastaSeqWindowed)
-	Ref.Kmer2coords = make(map[string]interval)
+	Ref := new(WindowedReference)
+	Ref.Kmer2coords = make(map[string][]Interval)
 	scanner := bufio.NewScanner(f)
 	var sb strings.Builder
 
@@ -62,7 +77,8 @@ func LoadRefWindowed(fasta_path string, k int) *FastaSeqWindowed {
 	for i := 0; i < len(seq) - k; i++ {
 		// remember: sliced sequence is 0-based, half-open
 		// genomic interval is 1-based, closed
-		Ref.Kmer2coords[seq[i:i+k]] = interval{i+1, i+k}
+		// Ref.Kmer2coords[seq[i:i+k]] = Interval{i+1, i+k}
+		Ref.addseq(seq[i:i+k], Interval{i+1, i+k})
 	}
 	return Ref
 }
--- workflows/src/fastaseq/fastaseq_test.go
+++ workflows/src/fastaseq/fastaseq_test.go
@@ -1,34 +1,41 @@
 package fastaseq_test
 import (
-	// "fmt"
-	// "bufio"
-	// "os"
+	"reflect"
 	"testing"
 	"annotation/fastaseq"
 )
 
-/*
-   Test the LoadRefWindowed function with the following input
-   test_ref_windows.fa
-   >CONTIG_NAME
-   ATCGAATT
-   TGAATGTA
- */
-func TestLoadRefWindows(t *testing.T)() {
-	Ref := fastaseq.LoadRefWindowed("test_ref_windows.fa", 3)
-	
-	// Test contig field
-	a := "CONTIG_NAME"
-	b := Ref.Contig
-	if a != b {
-		t.Errorf("contig check: exptected %s but got %s", a, b)
-	}
+// Test the WindowedReference struct/methods with the following fasta:
+// test_ref_windows.fa (in same working directory)
+func TestWindowedReference(t *testing.T)() {
+	Ref := fastaseq.LoadWindowedReference("test_ref_windows.fa", 3)
 
-	// Test ATC: unique kmer
-	a1, a2 := 1, 3
-	b1, b2 := Ref.Query()
-	// TODO Need to make the query return a list of possible regions? yes
-
-
-	// Test GAA: non-unique kmer
+	t.Run("Contig Parsed", func(t *testing.T)() {
+		a := "CONTIG_NAME"
+		b := Ref.Contig
+		if a != b {
+			t.Errorf("contig check: exptected %s but got %s", a, b)
+		}
+	})
+	t.Run("Unique Kmer", func(t *testing.T)() {
+		result := Ref.Query("ATC")
+		correct := fastaseq.Interval{1, 3}
+		if len(result) != 1 {
+			t.Errorf(
+				"Query must have only contain 1 interval.  %d found",
+				len(result))
+		} else if result[0] != correct {
+			t.Errorf(
+				"correct = %+v; result[0] = %+v", correct, result[0])
+		}
+	})
+	t.Run("Non-Unique Kmer", func(t *testing.T)() {
+		result := Ref.Query("GAA")
+		correct := []fastaseq.Interval{{4, 6}, {10, 12}}
+		if !reflect.DeepEqual(result, correct) {
+			t.Errorf("\nQuery: 'GAA'\nresult=%v\ncorrect=%v,", result, correct)
+		}
+		result = Ref.Query("AAT")
+		correct = []fastaseq.Interval{{5, 7}, {11, 13}}
+	})
 }
