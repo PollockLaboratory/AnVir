782fe512780561f1dad3bbba73ad5c6c85001b3b
Murad <mchowdh200@gmail.com>
2022-04-28 15:51:41 -0600

    add amino acid change/tests

---
+++ workflows/src/amino/amino.go
@@ -0,0 +1,264 @@
+// This package will determine amino acid changes for a single variant.
+// See tests for conceptual examples.
+package amino
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+
+	"annotation/fastaseq"
+	. "annotation/utils"
+	"annotation/vcf"
+)
+
+type change struct {
+	from string
+	to string
+
+	// codon number of where change took place
+	// these use 1-based indexing.  Don't you
+	// just love mixing 1 and 0-based indexing?!
+	at int
+}
+func (c change)str() string {
+	return fmt.Sprintf("%d%s>%s", c.at, c.from, c.to)
+}
+
+// return comma separated string of formatted change strings
+func formatChanges(changes []change) string{
+	change_strings := make([]string, 0, len(changes))
+	for i := range changes {
+		change_strings = append(change_strings, changes[i].str())
+	}
+	return strings.Join(change_strings, ",")
+}
+
+// Get the 0-based index position wrt the start pos,
+// Assuming start and pos have same indexing offset.
+func RelativeCoords(start int, pos int) int {
+	return pos - start
+}
+
+// given 1-based start of gene and genomic pos query,
+// get the 0-based codon index.
+func CodonPos(start int, pos int) int {
+	return RelativeCoords(start, pos) / 3
+}
+
+// given gene start & genomic interval (1-based),
+// get the range of codon indices (0-based).
+func CodonRange(gstart int, istart int, iend int) (int, int) {
+	return CodonPos(gstart, istart), CodonPos(gstart, iend)
+}
+
+// Convert codon index to start/end of query.
+//// inputs:
+// cindex: codon index used as offset from start of gene
+// gstart: start coordinate of gene (1-based)
+//// outputs:
+// int: start coord (1-based) in reference genome
+func CodonIndex2Genomic(cindex int, gstart int) int {
+	return gstart + cindex*3
+}
+
+// given ref genome, gene start (1based), codon index range (0 based... yep)
+// get the codons' genomic sequence
+func CodonSeq(ref *fastaseq.ContiguousReference,
+		gstart int, cstart int, cend int) string {
+	// 1
+	// |||___|||___|||___
+	//  0  1  2  3  4  5
+	// say cstart = 2; cend = 4. we need 3 codons
+	// so 4-2+1 = 3
+	var seq strings.Builder
+	// n := cend-cstart+1
+	for i := cstart; i <= cend; i++ {
+		// convert codon index to start/end of query
+		// gstart + 3*cstart , gstart + 
+		gpos := CodonIndex2Genomic(i, gstart)
+		seq.WriteString(ref.Query(gpos, gpos+2))
+	}
+	return seq.String()
+}
+
+
+func GetCodonTable(codons_file string) map[string]byte {
+	f, err := os.Open(codons_file)
+	Check(err)
+	defer f.Close()
+
+	// codon -> amino acid
+	table := make(map[string]byte)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		table[fields[0]] = fields[1][0]
+	}
+
+	return table
+}
+
+// apply a SNP to a codon's sequence given the variant's
+// relative position within the codon.
+func ApplySNP(codon_seq string, pos int, alt_seq byte) string {
+	var sb strings.Builder
+	for i := range codon_seq {
+		if i == pos {
+			sb.WriteByte(alt_seq)
+		} else {
+			sb.WriteByte(codon_seq[i])
+		}
+	}
+	return sb.String()
+}
+
+// apply a deletion to a provided codon_seq, given the relative start
+// point and the length of the variant.  Using closed interval.
+func ApplyDEL(codon_seq string, pos int, length int) string {
+	var sb strings.Builder
+	for i := 0; i < pos; i++ {
+		sb.WriteByte(codon_seq[i])
+	}
+	for i := pos; i < pos + length; i++ {}
+	for i := pos+length; i < len(codon_seq); i++ {
+		sb.WriteByte(codon_seq[i])
+	}
+	return sb.String()
+}
+
+// apply an insertion to a provided codon_seq, given the relative start
+// point and inserted sequence. Insertion begins AFTER pos.
+func ApplyINS(codon_seq string, pos int, inserted_seq string) string {
+	var sb strings.Builder
+	var i int
+	for i = 0; i <= pos; i++ {
+		sb.WriteByte(codon_seq[i])
+	}
+	for j := range inserted_seq {
+		sb.WriteByte(inserted_seq[j])
+	}
+	for ; i< len(codon_seq); i++ {
+		sb.WriteByte(codon_seq[i])
+	}
+	return sb.String()
+}
+
+// apply a compound variant to a provided codon_seq, given the relative start
+// point and ref/alt allele sequences. Variant begins AFTER pos.
+func ApplyCompound(codon_seq string, pos int, ref_seq string, alt_seq string) string {
+	var sb strings.Builder
+	var i int
+	for i = 0; i <= pos; i++ {
+		sb.WriteByte(codon_seq[i])
+	}
+	for j := 0; j < len(ref_seq); j++ {
+		if ref_seq[j] == alt_seq[j] { // matches
+			sb.WriteByte(ref_seq[j])
+			i++
+		} else if ref_seq[j] == '-' { // inserted base
+			sb.WriteByte(alt_seq[j])
+		} else if alt_seq[j] == '-' { // deleted base
+			i++
+		} else { // polymorphism
+			sb.WriteByte(alt_seq[j])
+			i++
+		}
+	}
+	for ; i < len(codon_seq); i++ {
+		sb.WriteByte(codon_seq[i])
+	}
+	return sb.String()
+}
+
+func DNA2AminoAcid(dna string, codon_table map[string]byte) string {
+	var sb strings.Builder
+	for i := 0; i + 3 <= len(dna); i+=3 {
+		sb.WriteByte(codon_table[dna[i:i+3]])
+	}
+	return sb.String()
+}
+
+func GetChanges(ref_aa , alt_aa string, cstart int) []change {
+	// align ref/alt aa
+	ref_align, alt_align, err := AlignSequences(ref_aa, alt_aa, false)
+	Check(err)
+
+	changes := make([]change, 0, len(ref_aa))
+	for i := range ref_align {
+		if ref_align[i] == '-' { // inserted AA
+			changes = append(changes,
+				change{from: "ins", to: string(alt_align[i]), at: cstart + i})
+		} else if alt_align[i] == '-' { // deleted AA
+			changes = append(changes,
+				change{from: string(ref_align[i]),
+					to: "del", at: cstart + i})
+		} else { // substitution
+			changes = append(changes,
+				change{from: string(ref_align[i]),
+					to: string(alt_align[i]), at: cstart + i})
+		}
+	}
+	return changes
+}
+
+// given and vcf record, gene start, get the amino acid changes
+func AminoAcidChanges(ref *fastaseq.ContiguousReference, record *vcf.Record,
+		gene_intervals map[string]Interval,
+		codon_table map[string]byte) (string, bool) {
+
+	// get relevant information from vcf line
+	pos := record.Pos
+	ref_seq := record.Ref
+	alt_seq := record.Alt
+	end , _:= strconv.Atoi(record.Info["END"][0])
+
+	gene := record.Info["GENE"][0]
+	gstart := gene_intervals[gene].Start
+
+	// Get codon index range
+	// TODO right now do limited range of codons.
+	// However, keep in mind that a frameshift will
+	// expand this range to the end of the gene.
+	// Luckily, none of the logic after this ought 
+	// to change if I decide to go that route.
+	cstart, cend := CodonRange(gstart, pos, end)
+
+	// get refseq spanning the codons
+	ref_codons := CodonSeq(ref, gstart, cstart , cend)
+
+	// get position of variant wrt to the first codon
+	var_codon_pos := RelativeCoords(gstart, pos) % 3
+	
+	// applied variants to the codon seq
+	var alt_codons string
+	switch record.Info["TYPE"][0] {
+	case "SNP":
+		alt_codons = ApplySNP(ref_codons, var_codon_pos, alt_seq[0])
+	case "DEL", "DEL_REPEAT":
+		alt_codons = ApplyDEL(ref_codons, var_codon_pos, pos-end+1)
+	case "INS":
+		alt_codons = ApplyINS(ref_codons, var_codon_pos, alt_seq)
+	case "COMPOUND":
+		alt_codons = ApplyCompound(ref_codons, var_codon_pos, ref_seq, alt_seq)
+	}
+	var frameshift bool
+	if len(alt_codons) % 3 != 0 {
+		frameshift = true
+	}
+
+	// get amino acid sequence of ref/alt
+	// TODO return a bool for shifted reading frame
+	// TODO OR write a function that finds the start of shifted reading frame
+	// TODO add info field for the frameshift (bool?)
+	// TODO if there is a shifted reading frame then
+	ref_aa := DNA2AminoAcid(ref_codons, codon_table)
+	alt_aa := DNA2AminoAcid(alt_codons, codon_table)
+
+	// get list of changes
+	changes := GetChanges(ref_aa, alt_aa, cstart)
+
+	return formatChanges(changes), frameshift
+}
---
+++ workflows/src/amino/amino_test.go
@@ -0,0 +1,237 @@
+// TODO put test data for all packages into a single directory
+package amino_test
+
+import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"sort"
+	"testing"
+
+	. "annotation/amino"
+	. "annotation/fastaseq"
+	. "annotation/utils"
+)
+
+func compare[T any](result T, correct T, t *testing.T) {
+	if !reflect.DeepEqual(result, correct) {
+		t.Errorf("\ncorrect: %+v\nresult: %+v\n", correct, result)
+	}
+}
+
+func TestRelativeCoords(t *testing.T) {
+	// yes this is probably a stupid test :)
+	// 0123456789
+	// ABCDEFGHIJ
+	// ^    ^
+	// 10   15
+	// relative
+	// 15-10 = 5
+	result := RelativeCoords(10, 15)
+	correct := 5
+	compare(result, correct, t)
+}
+
+func TestCodonPos(t *testing.T) {
+	// abs:  25       33
+	//        v       v  
+	// rel:   012345678
+	// gene:  |||___|||
+	// codon:  0  1  2
+	t.Run("start:25,pos:32", func(t *testing.T){
+		correct := 2
+		result := CodonPos(25, 32)
+		compare(result, correct, t)
+	})
+	t.Run("start:25,pos:28", func(t *testing.T){
+		correct := 1
+		result := CodonPos(25, 28)
+		compare(result, correct, t)
+	})
+	t.Run("start:25,pos:27", func(t *testing.T){
+		correct := 0
+		result := CodonPos(25, 27)
+		compare(result, correct, t)
+	})
+}
+
+func TestCodonRange(t *testing.T) {
+	// 1  4  7  10 13 16 19
+    // v  v  v  v  v  v  v
+	// |||___|||___|||___|||
+	//  0  1  2  3  4  5  6
+	r1, r2 := CodonRange(1, 5, 13)
+	c1, c2 := 1, 4
+	compare(r1, c1, t)
+	compare(r2, c2, t)
+}
+
+func TestCodonIndex2Genomic(t *testing.T) {
+	// gstart
+	// v
+	// |||___|||
+	// 123456789 <- genomic coordinates
+	//  0  1  2  <- cindex
+	// eg gstart = 1, cindex = 1
+	// then start = 1 + 1*3 = 4
+	correct := 4
+	result := CodonIndex2Genomic(1, 1)
+	compare(result, correct, t)
+}
+
+func TestGetCodonSeq(t *testing.T) {
+	// example:
+	//
+	//    4 <-gstart (genomic start pos of gene)
+	//    v
+	// AAABBBCCCDDDEEE
+	// FFFGGGHHHIIIJJJ
+	//     ^     ^
+	//     5     7 <- codon indices
+	ref := LoadContiguousReference("test_data/test.fa")
+	correct := "GGGHHHIII"
+	result := CodonSeq(ref, 4, 5, 7)
+	compare(correct, result, t)
+}
+
+func TestGetCodonTable(t *testing.T) {
+	path, err := filepath.Abs("../../data/dna_codon_table.tsv")
+	Check(err)
+	table := GetCodonTable(path)
+
+	// test by inspection only
+	// for ease of comparison store keys in sorted order
+	keys := make([]string, 0, len(table))
+	for k := range table {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Println(k, string(table[k]))
+	}
+}
+
+func TestApplySNP(t *testing.T) {
+	codon_seq := "ABC"
+	
+	t.Run("pos=0", func(t *testing.T) {
+		correct := "XBC"
+		pos := 0
+		alt_seq := "X"
+		result := ApplySNP(codon_seq, pos, alt_seq[0])
+		compare(result, correct, t)
+	})
+	t.Run("pos=1", func(t *testing.T) {
+		correct := "AXC"
+		pos := 1
+		alt_seq := "X"
+		result := ApplySNP(codon_seq, pos, alt_seq[0])
+		compare(result, correct, t)
+	})
+	t.Run("pos=2", func(t *testing.T) {
+		correct := "ABX"
+		pos := 2
+		alt_seq := "X"
+		result := ApplySNP(codon_seq, pos, alt_seq[0])
+		compare(result, correct, t)
+	})
+}
+
+func TestApplyDEL(t *testing.T) {
+	codon_seq := "ABCDEFGHIJKLMNOPQRSTUVWX"
+	t.Run("pos=0;length=3", func(t *testing.T) {
+		correct := "DEFGHIJKLMNOPQRSTUVWX"
+		pos := 0
+		length := 3
+		result := ApplyDEL(codon_seq, pos, length)
+		compare(result, correct, t)
+	})
+	t.Run("pos=5;length=4", func(t *testing.T) {
+		correct := "ABCDEJKLMNOPQRSTUVWX"
+		pos := 5
+		length := 4
+		result := ApplyDEL(codon_seq, pos, length)
+		compare(result, correct, t)
+	})
+	t.Run("pos=5;length=4", func(t *testing.T) {
+		correct := "ABCDE"
+		pos := 5
+		length := 19
+		result := ApplyDEL(codon_seq, pos, length)
+		compare(result, correct, t)
+	})
+}
+
+func TestApplyINS(t *testing.T) {
+	codon_seq := "ABCDEFGHIJKLMNOPQRSTUVWX"
+	t.Run("pos=0;hello", func(t *testing.T) {
+		correct := "AhelloBCDEFGHIJKLMNOPQRSTUVWX"
+		pos := 0
+		result := ApplyINS(codon_seq, pos, "hello")
+		compare(result, correct, t)
+	})
+	t.Run("pos=5;hello", func(t *testing.T) {
+		correct := "ABCDEFhelloGHIJKLMNOPQRSTUVWX"
+		pos := 5
+		result := ApplyINS(codon_seq, pos, "hello")
+		compare(result, correct, t)
+	})
+	t.Run("pos=23;hello", func(t *testing.T) {
+		correct := "ABCDEFGHIJKLMNOPQRSTUVWXhello"
+		pos := 23
+		result := ApplyINS(codon_seq, pos, "hello")
+		compare(result, correct, t)
+	})
+}
+
+func TestApplyCompound(t *testing.T) {
+	codon_seq := "ABCDEFGHIJKLMNOPQRSTUVWX"
+	t.Run("pos=7;ref=HIJK;alt=#IJ#", func(t *testing.T) {
+		correct := "ABCDEFG#IJ#LMNOPQRSTUVWX"
+		pos := 6
+		ref := "HIJK"
+		alt := "#IJ#"
+		result := ApplyCompound(codon_seq, pos, ref, alt)
+		compare(result, correct, t)
+	})
+	t.Run("pos=7;ref=H--IJK;alt=H##IJ#", func(t *testing.T) {
+		correct := "ABCDEFGH##IJ#LMNOPQRSTUVWX"
+		pos := 6
+		ref := "H--IJK"
+		alt := "H##IJ#"
+		result := ApplyCompound(codon_seq, pos, ref, alt)
+		compare(result, correct, t)
+	})
+	t.Run("pos=7;ref=HIJKLMN;alt=HIJ--#-", func(t *testing.T) {
+		correct := "ABCDEFGHIJ#OPQRSTUVWX"
+		pos := 6
+		ref := "HIJKLMN"
+		alt := "HIJ--#-"
+		result := ApplyCompound(codon_seq, pos, ref, alt)
+		compare(result, correct, t)
+	})
+}
+
+func TestDNA2AminoAcid(t *testing.T) {
+	// small subset of codon table
+	codon_table := make(map[string]byte)
+	codon_table["TTT"] = 'F'
+	codon_table["TTG"] = 'L'
+	codon_table["TTC"] = 'F'
+	codon_table["TTA"] = 'L'
+	codon_table["TGT"] = 'C'
+	codon_table["TGG"] = 'W'
+
+	t.Run("divisible by 3", func(t *testing.T) {
+		dna := "TTTTTGTTCTTATGTTGG"
+		correct := "FLFLCW"
+		result := DNA2AminoAcid(dna, codon_table)
+		compare(result, correct, t)
+	})
+	t.Run("not divisible by 3", func(t *testing.T) {
+		dna := "TTTTTGTTCTTATGTTG"
+		correct := "FLFLC"
+		result := DNA2AminoAcid(dna, codon_table)
+		compare(result, correct, t)
+	})
+}
